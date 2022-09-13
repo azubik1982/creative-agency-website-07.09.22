1f73c5cb4b2b34163be13b3d41db54af126be46e
azubik1982 <azubik1982@gmail.com>
2022-09-13 20:13:14 +0300

    feat: do slider-top block in section-head

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(function(){
+    $('.top-slider').slick({
+        dots: true,
+        // speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+                arrows: false
+            }
+          }
+        ]
+      });
 
 });
